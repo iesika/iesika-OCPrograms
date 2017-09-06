add188fced8b2f5053591116b42f6f0b91db1444
iesika <info@iesika.net>
2017-09-06 15:11:14 +0900

    add floor_frame.lua

---
+++ floor_frame.lua
@@ -0,0 +1,96 @@
+--require oke lib
+local oke = require("oke")
+local sides = require("sides")
+
+local cfg = {
+    build_material = function(stack) end
+}
+--[[
+           ←length→ 
+    S → → → → → → → → → → → 
+S地点から開始し, 
+lengthの長さだけrobotの下にブロックを設置しながら移動する
+--]]
+local function line(length, material)
+    for i = 1, length do
+        oke.placeStack(sides.down, material, true)
+        if i != length then
+            oke.move(sides.down, true)
+        end
+    end
+end
+
+--[[
+                  ←col→ 
+        → → → → → → → → → → → → ↓
+        ↑                       ↓
+        ↑                       ↓
+        ↑                       ↓
+        ↑                       ↓  ↑
+        ↑                       ↓ row
+        ↑                       ↓  ↓
+        ↑                       ↓
+        ↑                       ↓
+        ↑                       ↓
+        ↑                       ↓
+        S ← ← ← ← ← ← ← ← ← ← ← ←
+上記のs地点から開始し，
+robotの下にブロックを設置しながらs地点まで周回した後，上（前）向きに戻る
+--]]
+local function frame(col, row, material)
+    line(row, material)
+    oke.turn(true)
+    line(col, material)
+    oke.turn(true)
+    line(row, material)
+    oke.turn(true)
+    line(col, material)
+    oke.turn(true)
+end
+
+local function make_farm_frame()
+    for i = 1, 3 do
+        for j = 1, 5 do
+            frame(13, 13, cfg.build_material)
+            oke.forward(12, true)
+        end
+        if i != 3 then
+            frame(13, 5, cfg.build_material)
+            oke.forward(5, true)
+        end
+    end
+    oke.back(188, true)
+end
+
+local function make_farm_scaffold()
+    for i = 1, 3 do
+        for j = 1, 5 do
+            frame(5, 13, cfg.build_material)
+            oke.forward(12, true)
+        end
+        if i != 3 then
+            frame(5, 5, cfg.build_material)
+            oke.forward(5, true)
+        end
+    end
+    oke.back(188, true)
+end
+
+local function go_next_frameline()
+    oke.turn(true)
+    oke.forward(12, true)
+    oke.turn(false)
+end
+
+local function main()
+    for i = 1, 3 do
+        make_farm_scaffold()
+        go_next_frameline()
+        make_farm_frame()
+        if not i != 3 then
+            go_next_frameline()
+        end
+    end
+end
+
+main()
